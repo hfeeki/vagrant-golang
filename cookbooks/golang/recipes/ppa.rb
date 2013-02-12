c691b352354e49f05b095ec64126b1315c553cdd
Michael <hfeeki@gmail.com>
2013-02-12 23:29:06 +0800

    Update cookbooks/golang/recipes/ppa.rb
    enable ppa install golang

--- cookbooks/golang/recipes/ppa.rb
+++ cookbooks/golang/recipes/ppa.rb
@@ -1,19 +1,25 @@
 include_recipe "build-essential"
 package "mercurial"
 
-bash "Fetch golang source" do
-  cwd "/home/vagrant"
-  code <<-EOC
-    hg clone -u release #{node['golang']['repo']}
-  EOC
-  creates "~/go"
+include_recipe "apt"
+
+# Add ppa apt repository for go
+apt_repository "gophers-go-ppa" do
+  uri          "http://ppa.launchpad.net/gophers/go/ubuntu"
+  distribution node['lsb']['codename']
+  components   ["main"]
+  key          "9AD198E9"
+  keyserver    "keyserver.ubuntu.com"
+  action :add
 end
 
-bash "Build golang" do
-  cwd "/home/vagrant/go/src"
-  code <<-EOC
-    ./all.bash
-  EOC
+execute "apt-get update" do
+  action :nothing
+end
+
+# Install go stable
+package "golang-stable" do
+  action :install
 end
 
 bash "Export ENV Vars" do
@@ -23,25 +29,4 @@ bash "Export ENV Vars" do
     source /home/vagrant/.bashrc
   EOC
 end
-
-#include_recipe "apt"
-#
-## Add ppa apt repository for go
-#apt_repository "gophers-go-ppa" do
-#  uri          "http://ppa.launchpad.net/gophers/go/ubuntu"
-#  distribution node['lsb']['codename']
-#  components   ["main"]
-#  key          "9AD198E9"
-#  keyserver    "keyserver.ubuntu.com"
-#  action :add
-#end
-#
-#execute "apt-get update" do
-#  action :nothing
-#end
-
-# Install go stable
-#package "golang-stable" do
-#  action :install
-#end
 
